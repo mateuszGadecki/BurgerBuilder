3daf8076b5e25eadcd6f9935e4981622a06a7780
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2020-12-14 21:12:02 +0100

    Adding removing ingredients method

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,7 +4,7 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  let transforedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map((igKey) => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
@@ -14,14 +14,14 @@ const burger = (props) => {
       return arr.concat(el);
     }, []);
 
-  if (transforedIngredients.length === 0) {
-    transforedIngredients = <p>Please start adding ingredients!</p>;
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
   }
 
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      {transforedIngredients}
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
