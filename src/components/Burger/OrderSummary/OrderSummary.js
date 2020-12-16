9060050cff9490152de546f7f612c212190e0a45
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2020-12-16 18:39:17 +0100

    Adding Buttons and Price to the Modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Aux from "../../../hoc/Aux";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -17,7 +18,16 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}$</strong>
+      </p>
       <p>Continue to Checkout?</p>
+      <Button clicked={props.purchaseCancelled} btnType="Danger">
+        Cancel
+      </Button>
+      <Button clicked={props.purchaseContinued} btnType="Success">
+        Continue
+      </Button>
     </Aux>
   );
 };
