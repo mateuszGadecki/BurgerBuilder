6a92f20a4136390240568662121ce67a331cf1c9
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2020-12-22 08:58:58 +0100

    Creating the Axios Instance and Sending a Post Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -83,7 +84,24 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You Continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Mateusz Gadecki",
+        address: {
+          street: "TestStreet 2",
+          zipCode: "33241",
+          country: "Poland",
+        },
+        email: "test@test.com",
+      },
+      deliveryMethot: "fastest",
+    };
+    axios
+      .post("orders.json", order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
 
   render() {
