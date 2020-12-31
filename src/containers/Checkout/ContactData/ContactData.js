3bc64ed73d26c44e2ce0eccd85f89c52f714b840
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2020-12-31 14:50:58 +0100

    Setting up a JS Config (Contact Data Form)

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,63 @@ import Input from "../../../components/UI/Input/Input";
 
 class ContactData extends Component {
   state = {
-    name: "",
-    email: "",
-    address: {
-      street: "",
-      postalCode: "",
+    orderForm: {
+      name: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your Name",
+        },
+        value: "",
+      },
+      street: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Street",
+        },
+        value: "",
+      },
+      zipCode: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "ZIP Code",
+        },
+        value: "",
+      },
+      country: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Country",
+        },
+        value: "",
+      },
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Your Mail",
+        },
+        value: "",
+      },
+      deliveryMethod: {
+        elementType: "select",
+        elementConfig: {
+          options: [
+            {
+              value: "fastest",
+              displayValue: "Fastest",
+            },
+            {
+              value: "cheapest",
+              displayValue: "Cheapest",
+            },
+          ],
+        },
+        value: "",
+      },
     },
     loading: false,
   };
@@ -23,16 +75,6 @@ class ContactData extends Component {
     let order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      customer: {
-        name: "Mateusz Gadecki",
-        address: {
-          street: "TestStreet 2",
-          zipCode: "33241",
-          country: "Poland",
-        },
-        email: "test@test.com",
-      },
-      deliveryMethot: "fastest",
     };
     axios
       .post("/orders.json", order)
